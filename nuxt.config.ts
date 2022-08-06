0d1296845ab732a29f32175071984b16f347680f
takuma-ru <kcattdev@gmail.com>
2022-08-06 17:34:58 +0900

    [Add] Head

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,19 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  ssr: false
+  ssr: false,
+
+  app: {
+    head: {
+      titleTemplate: '%s｜さぎょツイ',
+      title: 'さぎょツイ',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+      ],
+      link: [
+        { rel: 'icon', type: 'image/png', href: '/icon_only.png' } // これを追記する
+      ]
+    }
+  }
 })
