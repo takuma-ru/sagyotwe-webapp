71367a43209edff06914f9a3a6a0a62c9e8592c2
takuma-ru <kcattdev@gmail.com>
2022-08-13 10:25:03 +0900

    [Update] Icon comp

--- components/Icon.vue
+++ components/Icon.vue
@@ -7,30 +7,43 @@
 <script lang="ts" setup>
 import { useColorStore } from '../store/color'
 
-const props = defineProps({
-  size: {
-    type: String,
-    default: '24px'
-  },
-  color: {
-    type: String,
-    default: null
-  }
-})
+/* -- type, interface -- */
+export interface IProps {
+  size?: string
+  color?: string
+  fill?: boolean
+  wght?: number
+  grad?: number
+  opsz?: number
 
+}
+/* -- props, emit -- */
 /* -- store -- */
 const {
   color
 } = useColorStore()
 
 /* -- variable(ref, reactive, computed) -- */
+/* -- function -- */
+/* -- watch -- */
+/* -- life cycle -- */
+
+const props = withDefaults(defineProps<IProps>(), {
+  size: '24px',
+  color: null,
+  fill: false,
+  wght: 400,
+  grad: 0,
+  opsz: 48
+})
 
 </script>
 
 <style lang="scss" scoped>
 .material-symbols-rounded {
+  font-variation-settings: 'FILL' v-bind('props.fill ? 1 : 0'), 'wght' v-bind('props.wght'), 'GRAD' v-bind('props.grad'), 'opsz' v-bind('props.opsz');
   font-size: v-bind('props.size');
   color: v-bind('props.color ? props.color : color.theme.text');
-  user-select: none
+  user-select: none;
 }
 </style>
